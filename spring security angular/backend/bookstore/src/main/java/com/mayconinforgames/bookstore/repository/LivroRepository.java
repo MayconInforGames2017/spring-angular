5634c1470be623260b91110b7256933ea1da37bc
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-07-13 15:02:29 -0300

    implementando as interfaces

---
+++ spring security angular/backend/bookstore/src/main/java/com/mayconinforgames/bookstore/repository/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.mayconinforgames.bookstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mayconinforgames.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+
+}
